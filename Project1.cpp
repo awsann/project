44a8e254159b716f5e0fa517f8a19a6f5c91ee63
Аня Щербак <t23typ@gmail.com>
2024-11-11 13:07:24 +0200

    Добавьте файлы проекта.

---
+++ Project1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	bool res;
+	double x, y;
+n:cout << "Ввести x-->";
+	cin >> x;
+	cout << "x=" << x << endl;
+	cout << "Ввести y-->";
+	cin >> y;
+	cout << "y=" << y << endl;
+	res = (y <= 7) && (y >= x) && (y >= -x) || (y >= -7) && (y <= x) && (y <= -x);
+	if (res) cout << "Точка попадає в область" << endl;
+	else cout << "Точка НЕ попадає в область" << endl;
+	goto n;
+	return 0;
+}
